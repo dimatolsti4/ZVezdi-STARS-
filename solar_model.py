fa36f1a243810220663552de7a4a2127f2848a15
MaximKunets <kunets.ma@phystech.edu>
2020-11-09 20:34:56 +0300

    u

--- solar_model.py
+++ solar_model.py
@@ -15,10 +15,11 @@ def calculate_force(body, space_objects):
     """
 
     body.Fx = body.Fy = 0
+
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**(0.5)
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** (0.5)
         f = (gravitational_constant * body.m * obj.m) / r ** 2
         body.Fy += f * (obj.y - body.y) / r
         body.Fx += f * (obj.x - body.x) / r
@@ -30,15 +31,14 @@ def move_space_object(body, dt):
 
     **body** — тело, которое нужно переместить.
     """
-    for i in range(dt):
-        ax = body.Fx/body.m
-        body.x += body.Vx + (ax)/2
-        body.Vx += ax
+    n = 1000
+    ax = body.Fx / body.m
+    body.x += body.Vx*n +(ax*(n**2)) / 2
+    body.Vx += ax *n
 
-        ay = body.Fy/body.m
-        body.y += body.Vy + (ay)/2
-        body.Vy += ay
-
+    ay = body.Fy / body.m
+    body.y += body.Vy *n +(ay*(n**2)) / 2
+    body.Vy += ay*n
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
@@ -48,11 +48,11 @@ def recalculate_space_objects_positions(space_objects, dt):
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
-
-    for body in space_objects:
-        calculate_force(body, space_objects)
-    for body in space_objects:
-        move_space_object(body, dt)
+    for i in range(dt):
+        for body in space_objects:
+            calculate_force(body, space_objects)
+        for body in space_objects:
+            move_space_object(body, dt)
 
 
 if __name__ == "__main__":
